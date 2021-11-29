7f7aa7bc061c9a11aaad50a84348e6bf3e3faad7
OH-JOO-YEONG <brb1111@naver.com>
2021-11-30 07:12:10 +0900

    [Feat] 파코테4 - 카드 랜덩 생성 구현 및 마무리

--- facote4/src/js/index.js
+++ facote4/src/js/index.js
@@ -1,5 +1,8 @@
 import EventCard from "./components/eventCard.js";
+import Shuffling from "./components/shuffling.js";
 
+const shuffling = new Shuffling();
 const eventCard = new EventCard();
 
+shuffling.setup();
 eventCard.setup();
