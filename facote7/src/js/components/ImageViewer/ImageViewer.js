692c7ded15312147bcc34713fb657e89e8551c1e
OH-JOO-YEONG <brb1111@naver.com>
2021-12-07 12:26:45 +0900

    [Feat] 파코테7 - 브레드크럼 컴포넌트 작업

--- facote7/src/js/components/ImageViewer/ImageViewer.js
+++ facote7/src/js/components/ImageViewer/ImageViewer.js
@@ -1,13 +1,44 @@
 class ImageViewer {
-  constructor() {}
+  constructor(props) {
+    const { parentElement } = props;
+    this.parentElement = parentElement;
+    this.renderElement = ImageViewer.createImageViewer();
+    this.bindEvents();
+  }
 
-  static createImageViewer() {}
+  static createImageViewer() {
+    const imageViewerWrapper = document.createElement("section");
+    const imageContent = document.createElement("div");
+    const imageElement = document.createElement("img");
 
-  bindEvents() {}
+    imageViewerWrapper.classList.add("modal", "image-viewer");
+    imageContent.classList.add("content");
 
-  open() {}
+    imageContent.appendChild(imageElement);
+    imageViewerWrapper.appendChild(imageContent);
 
-  close() {}
+    return imageViewerWrapper;
+  }
+
+  bindEvents() {
+    this.renderElement.addEventListener("click", (event) => {
+      const currentTarget = event.target;
+
+      // 검은 배경을 클릭해야 닫힘
+      if (this.renderElement === currentTarget) {
+        this.close();
+      }
+    });
+  }
+
+  open(filePath = "") {
+    this.renderElement.querySelector("img").src = filePath;
+    this.parentElement.appendChild(this.renderElement);
+  }
+
+  close() {
+    this.parentElement.removeChild(this.renderElement);
+  }
 }
 
 export default ImageViewer;
